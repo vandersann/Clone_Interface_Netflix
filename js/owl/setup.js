5df4cd5d8ac780de62e46d1ca7f8027c4d81a445
Vander Sandino <65673565+vandersann@users.noreply.github.com>
2020-11-12 16:24:06 -0300

    Finalizando projeto

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,16 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     responsive:{
         0:{
-            items:1
-        },
-        600:{
             items:3
         },
+        600:{
+            items:4
+        },
         1000:{
-            items:5
+            items:8
         }
     }
 })
